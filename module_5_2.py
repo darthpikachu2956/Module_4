4de73321c5c1deba46e00e7a13bc52fd0de33c4c
Vlad <darthpikachu2956@gmail.com>
2024-07-16 22:25:29 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,28 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Name: {self.name}, number of floors: {self.number_of_floors}'
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('There is not such a floor in this house.')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+first_house = House('West Pines', 16)
+second_house = House('Grove Street', 2)
+first_house.go_to(5)
+second_house.go_to(4)
+print(first_house)
+print(second_house)
+print(len(first_house))
+print(len(second_house))
